7cac31b4617d0fde9c8186c66ced8822332eea43
Veloraxzia <impendingdoom.taco@gmail.com>
2021-02-15 00:43:52 -0700

    ;

--- matrix_static.h
+++ matrix_static.h
@@ -30,5 +30,5 @@ matrix negate(matrix);
 matrix multiply(matrix, matrix);
 matrix scalar_multiply(T scalar, matrix);
 /* remaining function prototypes not shown */
-matrix transpose(matrix)
+matrix transpose(matrix);
 #endif
