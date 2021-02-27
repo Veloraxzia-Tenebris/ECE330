e10506002f2caf952d9db5b3c60b3f32b1617dd6
Veloraxzia <impendingdoom.taco@gmail.com>
2021-02-26 22:00:30 -0700

    My commits are just memes at this point

--- HW3/heartrates.cpp
+++ HW3/heartrates.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream
 #include <string>
 #include <heartrates.hpp>
 using namespace std;
--- HW3/heartrates.hpp
+++ HW3/heartrates.hpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <string
 
 class HeartRates {
 	private:
