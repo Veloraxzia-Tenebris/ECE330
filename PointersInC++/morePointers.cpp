fba0f6fc79b124506d291adf328ccd1379b1a5ba
Veloraxzia <impendingdoom.taco@gmail.com>
2021-03-06 01:15:58 -0700

    Pointer to functions

--- PointersInC++/morePointers.cpp
+++ PointersInC++/morePointers.cpp
@@ -1,17 +1,21 @@
-// more pointers
+// pointer to functions
 #include <iostream>
 using namespace std;
-
-int main() {
-	int firstvalue = 5, secondvalue = 15;
-	int * p1, * p2;
-	p1 = &firstvalue; // p1 = address of firstvalue
-	p2 = &secondvalue; // p2 = address of secondvalue
-	*p1 = 10; // value pointed by p1 = 10
-	*p2 = *p1; // value pointed by p2 = value pointed by p1
-	p1 = p2; // p1 = p2 (value of pointer is copied)
-	*p1 = 20; // value pointed by p1 = 20
-	cout << "firstvalue is " << firstvalue << endl;
-	cout << "secondvalue is " << secondvalue << endl;
-	return 0;
+int addition (int a, int b)
+{ return (a+b); }
+int subtraction (int a, int b)
+{ return (a-b); }
+int operation (int x, int y, int (*functocall)(int,int))
+{
+int g;
+g = (*functocall)(x,y);
+return (g);
 }
+int main ()
+{
+int m,n;
+int (*minus)(int,int) = subtraction;
+m = operation (7, 5, addition);
+n = operation (20, m, minus);
+cout <<n;
+return 0;}
