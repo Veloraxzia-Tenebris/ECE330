973ef25ca5434b0699809cc15ae819e586d9084f
Veloraxzia <impendingdoom.taco@gmail.com>
2021-03-05 23:01:20 -0700

    Test for all types of employees

--- HW4/test.cpp
+++ HW4/test.cpp
@@ -3,24 +3,36 @@
 
 #include <iostream>
 #include <string>
+#include <exception>
 #include "employee.h"
 #include "employee.cpp"
-//#include "salariedemployee.h"
+#include "salariedemployee.h"
+#include "salariedemployee.cpp"
 #include "hourlyemployee.h"
 #include "hourlyemployee.cpp"
 using namespace std;
 
 int main() 
 {
-	HourlyEmployee e2(002, "Hendrix", "Jimi", "NMI", 14);
-//	e1 (001, "Jones", "Booker", "T", 22),
-//	e2 (002, "Hendrix", "Jimi", "NMI ", 14),
-//	e3 (003, "Morrison", "Jim", "D", 03);
-	
-	e2.setHours(10);
-	e2.setRate(20);
-	e2.setOvertime(5);
-
-//	e1.printEmployee();
+	SalariedEmployee e1(001, "Jones", "Booker", "T", 22), e2(002, "Hendrix", "Jimi", "NMI", 14);
+	HourlyEmployee e3(003, "Morrison", "Jim", "D", 03), e4(004, "Iwork", "Overtime", "XYZ", 42);
+	float salary = 100;
+	float rate = 20;
+	int hours = 10;
+	e1.setSalary(salary);
+	e2.setSalary(salary);
+	e2.setWorked(0.5);
+	e3.setHours(hours);
+	e3.setRate(rate);
+	e4.setHours(hours);
+	e4.setRate(rate);
+	e4.setOvertime(5);
+	cout << endl << "> > > Full-time salary: ";
+	e1.printEmployee();
+	cout << endl << "> > > Half-time salary: ";
 	e2.printEmployee();
+	cout << endl << "> > > Hourly worker no overtime: ";
+	e3.printEmployee();
+	cout << endl << "> > > Hourly worker overtime: ";
+	e4.printEmployee();
 }
