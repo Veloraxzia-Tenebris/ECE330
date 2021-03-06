b717f0329c69771637728c076402e3069f3961d3
Veloraxzia <impendingdoom.taco@gmail.com>
2021-03-05 23:10:55 -0700

    Example 2

---
+++ Lab1/myclass.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class MyClass {
+	private:
+		int counter;
+	public:
+		void Foo() {
+			std::cout << "Foo" << std::endl;
+		}
+		void Foo() const {
+			std::cout << "Foo const" << std::endl;
+		}
+};
+
+int main() {
+	MyClass cc;
+	const MyClass ccc = cc;
+	cc.Foo();
+	ccc.Foo();
+}
