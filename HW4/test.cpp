05c3e4336f9176e25538612b1c2dbe5f78446656
Veloraxzia <impendingdoom.taco@gmail.com>
2021-03-05 22:29:13 -0700

    Tested hourly employee

--- HW4/test.cpp
+++ HW4/test.cpp
@@ -1,21 +1,26 @@
 /* File: test.cpp */
 // File to test the basic employee class
 
-#include<iostream>
+#include <iostream>
 #include <string>
 #include "employee.h"
-
+#include "employee.cpp"
+//#include "salariedemployee.h"
+#include "hourlyemployee.h"
+#include "hourlyemployee.cpp"
 using namespace std;
 
 int main() 
 {
-	Employee
-	e1 (001, "Jones", "Booker", "T", 22),
-	e2 (002, "Hendrix", "Jimi", "NMI ", 14),
-	e3 (003, "Morrison", "Jim", "D", 03);
+	HourlyEmployee e2(002, "Hendrix", "Jimi", "NMI", 14);
+//	e1 (001, "Jones", "Booker", "T", 22),
+//	e2 (002, "Hendrix", "Jimi", "NMI ", 14),
+//	e3 (003, "Morrison", "Jim", "D", 03);
 	
-	e1.printEmployee();
-	e2.printEmployee();
-	
+	e2.setHours(10);
+	e2.setRate(20);
+	e2.setOvertime(5);
 
+//	e1.printEmployee();
+	e2.printEmployee();
 }
