c342c16fa993fd2e53f71ea72a04aec17a74975d
Veloraxzia <impendingdoom.taco@gmail.com>
2021-03-13 00:09:10 -0700

    Finished homework

--- HW5/main.cpp
+++ HW5/main.cpp
@@ -2,6 +2,12 @@
 #include <iomanip>
 using namespace std;
 
+template <typename T> void swap(T* element1Ptr, T* element2Ptr) {
+	T hold = *element1Ptr;
+	*element1Ptr = *element2Ptr;
+	*element2Ptr = hold;
+}
+
 template <typename T> void selectionSort(T* const array, int const size) {
 	int smallest;
 	for(int i = 0; i < size - 1; ++i) {
@@ -13,15 +19,10 @@ template <typename T> void selectionSort(T* const array, int const size) {
 	}
 }
 
-template <typename T> void swap(T* const element1Ptr, T* const element2Ptr) {
-	T hold = *element1Ptr;
-	*element1Ptr = *element2Ptr;
-	*element2Ptr = hold;
-}
-
 template <typename T> void printarray(T* const array, int const size) {
 	for (int i = 0; i < size; ++i)
-		cout << array[i];
+		cout << setw(8) << array[i];
+	cout << endl;
 }
 
 int main() {
@@ -29,7 +30,7 @@ int main() {
 	int array1[arraySize] = {84, 56, 50, 33, 44, 53, 62, 30, 92, 94};
 	float array2[arraySize] = {68.99, 52.95, 71.96, 68.98, 48.39, 87.85, 49.58, 76.77, 80.26, 34.6};
 
-	cout << "Original data: " << setw(4) << endl;
+	cout << "Original data: " << endl;
 	cout << "Int array: " << endl;
 	printarray<int>(array1, arraySize);
 	cout << "Float array: " << endl;
